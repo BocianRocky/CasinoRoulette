38bb624ad789b8966a934e1eda3b77cd02451a82
BocianRocky <mateusz.ro@op.pl>
2024-08-07 13:12:51 +0200

    Add GetHotAndColdNumberFromSpins

--- CasinoRoulette/CasinoRoulette/Services/SpinService.cs
+++ CasinoRoulette/CasinoRoulette/Services/SpinService.cs
@@ -1,4 +1,5 @@
-﻿using CasinoRoulette.Models;
+﻿using CasinoRoulette.DTO;
+using CasinoRoulette.Models;
 using CasinoRoulette.Repositories;
 
 namespace CasinoRoulette.Services;
@@ -6,6 +7,7 @@ namespace CasinoRoulette.Services;
 public interface ISpinService
 {
     Task<Spin> CreateNewSpin();
+    Task<HotAndColdNumbersDto> GetHotAndColdNumberFromSpins();
 }
 
 public class SpinService : ISpinService
@@ -27,6 +29,31 @@ public class SpinService : ISpinService
         await _spinRepository.AddSpin(newSpin);
         await _spinRepository.SaveChanges();
         return newSpin;
+    }
+
+    public async Task<HotAndColdNumbersDto> GetHotAndColdNumberFromSpins()
+    {
+        var nums =await _spinRepository.GetWinnerNumbers();
+
+        if (nums == null || nums.Count<3)
+        {
+            nums = new List<int>();
+            var random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                nums.Add(random.Next(0,37));
+            }
+        }
+        
+        var hotNums = nums.Take(3).ToList();
+        var coldNums = nums.TakeLast(3).ToList();
+
+        var hotAndCold = new HotAndColdNumbersDto()
+        {
+            HotNumbers = hotNums,
+            ColdNumbers = coldNums
+        };
+        return hotAndCold;
     }
     
     

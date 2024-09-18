811c91cbb1ed9c5c91075f73a648f1d156f95676
BocianRocky <mateusz.ro@op.pl>
2024-09-18 18:00:40 +0200

    create AdminService

---
+++ CasinoRoulette/CasinoRoulette/Services/AdminService.cs
@@ -0,0 +1,40 @@
+﻿using CasinoRoulette.DTO;
+using CasinoRoulette.Repositories;
+
+namespace CasinoRoulette.Services;
+
+public interface IAdminService
+{
+    Task<List<PlayerProfitDto>> GetDataPlayersAndProfit();
+}
+
+public class AdminService : IAdminService
+{
+    private readonly IAdminRepository _adminRepository;
+
+    public AdminService(IAdminRepository adminRepository)
+    {
+        _adminRepository = adminRepository;
+    }
+
+    public async Task<List<PlayerProfitDto>> GetDataPlayersAndProfit()
+    {
+        var players =await _adminRepository.GetAllPlayersAndProfit();
+        if (!PlayersExists(players))
+        {
+            throw new InvalidOperationException("Error with player data");
+        }
+        return players;
+    }
+
+    private static bool PlayersExists(List<PlayerProfitDto> players)
+    {
+        if (players == null)
+        {
+            return false;
+        }
+
+        return true;
+    }        
+    
+}

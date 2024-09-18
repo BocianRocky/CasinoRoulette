f1172711ad936f93a104bc32198e8a22ebea7725
BocianRocky <mateusz.ro@op.pl>
2024-09-18 17:59:23 +0200

    add PlayerProfitDto

---
+++ CasinoRoulette/CasinoRoulette/DTO/PlayerProfitDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CasinoRoulette.DTO;
+
+public class PlayerProfitDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Telephone { get; set; }
+    public decimal AccountBalance { get; set; }
+    public decimal Profit { get; set; }
+    
+}
